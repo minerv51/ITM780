b9c4be92d87baddf9ed549b7d08b6a3038163d69
minerv51 <bam0119@hotmail.com>
2024-10-07 17:54:24 -0400

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+//Gets the nameButton element and activates the promptName function once it has been clicked
+document.getElementById('nameButton').addEventListener('click', promptName);
+
+//Define the function that prompts the user for their name
+function promptName() {
+    //Prompt the user for their name
+    let userName = prompt('Please enter your name: ')
+
+    //Check if they have entered a name
+    if (userName) {
+        //If so, greet them by their name
+        document.getElementById('nameButton').textContent = `Hello ${userName}!`
+    } else {
+        //If not, let them know no name was entered
+        document.getElementById('nameButton').textContent = 'No name was entered!'
+    }
+}
+
+//Gets the clickableImg element as Image
+const image = document.getElementById('clickableImg')
+
+//Add an event listener to the function enlargeImg to enlarge the image when clicked
+image.addEventListener('click', enlargeImg)
+
+//Define the function that enlarges the image once it has been clicked
+function enlargeImg() {
+    image.classList.toggle('enlarged');
+}
+
+//Function for dropping down the first dropdown menu
+function dropFunc1() {
+    document.getElementById("dropdown-1").classList.toggle("show");
+    }
+
+//Function for dropping down the second dropdown menu
+function dropFunc2() {
+    document.getElementById("dropdown-2").classList.toggle("show");
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = raiseFunc()
+
+function raiseFunc(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+}
+}
+
+//Highlight an element (works with onmouseover to highlight elements that are being hovered)
+function highlight(element) {
+    element.style.backgroundColor = "#a3c2ff"
+}
+
+//Resets an element (works with onmouseout to reset elements that are no longer being hovered)
+function resetItem(element) {
+    element.style.backgroundColor = ""
+}
