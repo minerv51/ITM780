fbf819e17c391b4c4c280e9cee072994304d96c2
minerv51 <bam0119@hotmail.com>
2024-11-13 23:38:14 -0500

    Lab 8

--- assets/script.js
+++ assets/script.js
@@ -37,7 +37,7 @@ function dropFunc2() {
     document.getElementById("dropdown-2").classList.toggle("show");
 }
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = raiseFunc()
+window.onclick = raiseFunc;
 
 function raiseFunc(event) {
     if (!event.target.matches('.dropbtn')) {
@@ -69,4 +69,41 @@ function relayPassword() {
     var password = document.getElementById("password").value;
     //Relay the password back to the user
     alert(`Your password is ${password}`)
+}
+
+// Define isIncreased
+let isIncreased = false;
+
+// Function to change the font size so it is more accessible
+function changeFontSize() {
+    //Select all body elements
+    const elements = document.querySelectorAll("body *");
+    elements.forEach(element => {
+        // Get the current font size for each element
+        let currentSize = window.getComputedStyle(element).fontSize;
+        // Convert the font size to a number and increase or decrease it depending on if it is already increased
+        let newSize = parseFloat(currentSize) + (isIncreased ? -4 : 4);
+        element.style.fontSize = newSize + "px";
+    });
+    // Change whether the font size is increased or not
+    isIncreased = !isIncreased;
+}
+
+//Select the change background colour button by its ID
+const button = document.getElementById("change-backg-colour")
+
+//Define isBackColourChanged
+let isBackColourChanged = false
+
+//Function to change the background colour
+function changeBackgroundColour () {
+    const newBackgroundColor = "#004949";
+    const originalBackgroundColor = "#6983b7"; // Assuming white is the original color
+
+    // Check the background color and change accordingly
+    document.body.style.backgroundColor = isBackColourChanged ? originalBackgroundColor : newBackgroundColor;
+
+    //Toggle background colour change
+    isBackColourChanged = !isBackColourChanged
+
 }
